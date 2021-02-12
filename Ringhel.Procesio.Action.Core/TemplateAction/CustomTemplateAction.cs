c31bbe724e5d0700323241f3adce9adfb99496b7
Alexandru Gal <alexandru.gal@procesio.com>
2021-02-12 20:55:37 +0200

    Update CustomTemplateAction.cs

--- Ringhel.Procesio.Action.Core/TemplateAction/CustomTemplateAction.cs
+++ Ringhel.Procesio.Action.Core/TemplateAction/CustomTemplateAction.cs
@@ -1,57 +1,28 @@
-﻿using Ringhel.Procesio.Action.Core.ActionDecorators;
-using Ringhel.Procesio.Action.Core.Models;
+﻿using Ringhel.Procesio.Action.Core;
+using Ringhel.Procesio.Action.Core.ActionDecorators;
 using Ringhel.Procesio.Action.Core.Utils;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace Ringhel.Procesio.Action.Core.TemplateAction
+namespace TestAction
 {
-    [ClassDecorator(Name = "Custom Template Action", Shape = ActionShape.Circle, Description = "Custom Template Action Description", Classification =Classification.CAT_1)]
-    [FEDecorator(Label = "Configuration Modal", Type = FeComponentType.Modal, Parent = "Config_Modal", Tab ="Input Tab")]
-    public sealed class CustomTemplateAction : IAction
+    [ClassDecorator(Name = "Custom Template Action", Shape = ActionShape.Circle, Description = "Custom Template Action Description", Classification = Classification.CAT_1)]
+    [FEDecorator(Label = "Configuration Modal", Type = FeComponentType.Modal, Parent = "Config_Modal", Tab = "Input Tab")]
+    public class MyCustonAction : IAction
     {
+
         #region Options
-        /// <summary>
-        /// Always use static IEnumerable for Options lists.
-        /// </summary>
-        private static IEnumerable<OptionModel> ConfigP1Options { get; } = new List<OptionModel>() 
-        { 
-            new OptionModel()
-            {
-                Name = "Option 1",
-                Value = "Value for option 1"
-            },
-            new OptionModel(){
-                Name = "Option 2",
-                Value = "Value for option 2"
-            }
-        };
-        /// <summary>
-        /// Always use static IEnumerable for Options lists.
-        /// </summary>
-        private static IEnumerable<OptionModel> ConfigP2Options { get; } = new List<OptionModel>()
-        {
-            new OptionModel()
-            {
-                Name = "Option 3",
-                Value = "Value for option 3"
-            },
-            new OptionModel(){
-                Name = "Option 4",
-                Value = "Value for option 4"
-            }
-        };
+        private const string ConfigP1Options = "[{\"Name\":\"my name1\",\"Value\":\"my name value 1\"},{\"Name\":\"my name2\",\"Value\":\"my name value 2\"}]";
+        private const string ConfigP2Options = "[{\"Name\":\"my name3\",\"Value\":\"my name value 3\"},{\"Name\":\"my name4\",\"Value\":\"my name value 4\"}]";
         #endregion
 
         #region Properties
-        [FEDecorator(Label = "FE Input property", Type = FeComponentType.Select, Tab = "Input Tab")]
+        [FEDecorator(Label = "FE Input property", Type = FeComponentType.Select, Tab = "Input Tab", DefaultValue = "1")]
         [BEDecorator(IOProperty = Direction.Input)]
         [Validator(IsRequired = false)]
         public string Input1 { get; set; }
 
-        [FEDecorator(Label = "FE Output property", Type = FeComponentType.Number, Tab = "Output Tab")]
+        [FEDecorator(Label = "FE Output property", Type = FeComponentType.Number, Tab = "Output Tab", DefaultValue = "2")]
         [BEDecorator(IOProperty = Direction.Output)]
         [Validator(IsRequired = true, Expects = ExpectedType.Number)]
         public int Output1 { get; set; }
